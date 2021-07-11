6afbe42633434d8745891e9b8ad5d5cf603b325c
otaree <deep.yen.ur.hash@gmail.com>
2021-07-11 17:45:26 +0530

    removed commented out image links

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -10,53 +10,6 @@ import {
 import GalleryImage from '../components/GalleryImage';
 import { baseURL } from "../constants";
 
-// const pictures = [
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/classroom.jpeg",
-//         title: "Classroom"
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/assembly.jpeg",
-//         title: "Morning Assembly"
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/prize-distribution.jpeg",
-//         title: "Prize Distribution"
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/yoga.jpeg",
-//         title: "Student Doing Yoga"
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/morning-assemly.jpeg",
-//         title: "Faculty during Morning Assembly"
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/back-assembly.jpeg",
-//         title: "Student during Morning Assembly"
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/culture-event.jpeg",
-//         title: "Student and teachers during culture event"
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/indepandence-day.jpeg",
-//         title: "Independence day"
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/shark-suracksha.jpeg",
-//         title: `Sadak Suraksha Jeevan Raksha`
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/teacher-day.jpeg",
-//         title: `Teacher Day`
-//     },
-//     {
-//         img: "https://ghss-wakro.s3.ap-south-1.amazonaws.com/soap.jpeg",
-//         title: `Following SOP during COVID-19`
-//     },
-// ];
-
 export default function Gallery() {
     const [pictures, setPictures] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
