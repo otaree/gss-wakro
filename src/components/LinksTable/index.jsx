75bfb10d3603cbe48cbbf5b8e10e84d95d2567a1
otaree <deep.yen.ur.hash@gmail.com>
2021-07-11 17:33:21 +0530

    debugged the link problem in mobile view

--- src/components/LinksTable/index.jsx
+++ src/components/LinksTable/index.jsx
@@ -54,7 +54,7 @@ export default function LinksTable({
                     {title}
                 </Text>
                 {
-                    links.map(({ title, link }, i) => {
+                    links.map(({ title, value: link }, i) => {
                         const isLast = (links.length - 1) === i;
                         return (
                             <Text key={i} p={2} fontSize="md" borderBottomWidth={isLast ? 0 : 0.75} borderBottomColor="gray.200">
